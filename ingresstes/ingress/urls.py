7427d07cacba9d848e13e28a6b4964895c87f326
Amar Dongardive <adongardive@ACMDC.com>
2025-07-16 15:50:30 +0530

    add white noise middle ware

--- ingresstes/ingress/urls.py
+++ ingresstes/ingress/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import k8s_ingress_view
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
-    path('', k8s_ingress_view, name='k8s_ingress'),
+    path('', k8s_ingress_view, name='k8s_ingress')+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 ]
--- ingresstes/ingresstes/settings.py
+++ ingresstes/ingresstes/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddlewar'
 ]
 
 ROOT_URLCONF = 'ingresstes.urls'
