6932e0c83e7caa05a5a3f9316b0f423a9593a1a0
Amar Dongardive <adongardive@ACMDC.com>
2025-07-16 16:20:59 +0530

    resolve the error related to static file

--- ingresstes/ingress/urls.py
+++ ingresstes/ingress/urls.py
@@ -4,5 +4,6 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('', k8s_ingress_view, name='k8s_ingress')+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-]
+    path('', k8s_ingress_view, name='k8s_ingress')
+    
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
